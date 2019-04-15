3094678217a4e7452128dd90db70d8cfd843f57e
Ferdinand van Wyk <fvanwyk3@bloomberg.net>
2019-04-15 13:46:34 +0100

    Use samling for testing

--- demo-flask/index.py
+++ demo-flask/index.py
@@ -11,7 +11,7 @@ from onelogin.saml2.utils import OneLogin_Saml2_Utils
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'onelogindemopytoolkit'
-app.config['SAML_PATH'] = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'saml')
+app.config['SAML_PATH'] = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'demo-flask/saml')
 
 
 def init_saml_auth(req):
@@ -125,4 +125,4 @@ def metadata():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
